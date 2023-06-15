0e220808bc4d9eb8b771187d4c32f5301f5e726f
badrsh980 <Badrsh980@gmail.com>
2023-06-15 12:29:50 +0300

    1

--- resources/views/vendor/mail/html/button.blade.php
+++ resources/views/vendor/mail/html/button.blade.php
@@ -1,21 +1,23 @@
 @props(['url', 'color' => 'primary', 'align' => 'center'])
-<table class="action" align="{{ $align }}" width="100%" cellpadding="0" cellspacing="0" role="presentation">
+<tab l class"action" align="{{ $align }}" width="100%" cellpadding="0" cellspacing="0" role="presentation">
     <tr>
         <td align="{{ $align }}">
-            <table width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation">
+            table width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation">
                 <tr>
                     <td align="{{ $align }}">
-                        <table border="0" cellpadding="0" cellspacing="0" role="presentation">
+                       <table border="0" cellpadding="0" cellspacing="0" role="presentation">
                             <tr>
                                 <td>
-                                    <a href="{{ $url }}" class="button button-{{ $color }}"
+                                   <a href="{{ $url }}" class="button button-{{ $color }}"
                                         target="_blank" rel="noopener">{{ $slot }}</a>
-                                </td>
+                                        
+                               </td>
                             </tr>
                         </table>
-                    </td>
+                   </td>
                 </tr>
             </table>
         </td>
+
     </tr>
 </table>
